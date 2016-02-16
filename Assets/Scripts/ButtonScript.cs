4ad7ad34d65bf89c7d49c89b71620cdf204c1108
Joseph Whitworth <jwhitworth@fullsail.edu>
2016-02-16 17:37:00 -0500

    Initial File Commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+    bool happybool = true;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ButtonClick(bool happybool)
+    {
+        if (!happybool)
+        {
+            Debug.Log("YAY false");
+
+        }
+
+
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Game Made By Team Production Class Team Awesome!!!");
+    }
+}
+
+
